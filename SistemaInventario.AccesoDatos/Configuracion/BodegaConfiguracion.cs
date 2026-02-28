67a8196cdc4edf298e368f9516a0a07ac16d2c6b
jeffer-c <jeffer-c@hotmail.com>
2026-02-28 01:47:30 +0100

    Configuración del Proyectofirst commit

---
+++ SistemaInventario.AccesoDatos/Configuracion/BodegaConfiguracion.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SistemaInvetario.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Configuracion
+{
+    class BodegaConfiguracion : IEntityTypeConfiguration<Bodega>
+    {
+        public void Configure(EntityTypeBuilder<Bodega> builder)
+        {
+            builder.HasKey(b => b.Id);
+            builder.Property(b => b.Nombre).IsRequired().HasMaxLength(60);
+            builder.Property(b => b.Descripcion).IsRequired().HasMaxLength(200);
+            builder.Property(b => b.Estado).IsRequired();
+        }
+    }
+}
---
+++ SistemaInvetario.Modelos/Bodega.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInvetario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Nombre es requerido")]
+        [MaxLength(60, ErrorMessage = "El nombre no puede tener mas de 60 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "La descripción es requerida")]
+        [MaxLength(200, ErrorMessage = "La descripción no puede tener mas de 200 caracteres")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El estado es requerido")]
+        public string Estado { get; set; }
+    }
+}
